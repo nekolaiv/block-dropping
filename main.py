fcc2db30f01f1da198b5eca5379279acb5ac8f7f
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-09-27 22:56:43 +0800

    add block falling test

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import os
+from time import sleep
+
+def clear():
+    os.system('clear')
+    
+def delay():
+    sleep(0.5)
+
+
 class Block:
     def __init__(self):
-        pass
+        self.block = '[]'
         
 
 class Components:
@@ -9,13 +19,13 @@ class Components:
        
         
 class Screen:
-    def __init__(self):
+    def __init__(self, screen_width=10, screen_height=20) -> None:
         self.screen_body = []
-        self.screen_width = 10
-        self.screen_height = 20
+        self.screen_width = screen_width
+        self.screen_height = screen_height
     
     
-    def display_edge(self, x, y):
+    def display_edge(self, x, y) -> None:
         print('\t<!', end='')
         for i in range(x * 2 + 1):
             print('=', end='')
@@ -24,17 +34,17 @@ class Screen:
     
     def set_screen_body(self) -> None:
         for i in range(self.screen_height):
-            element = ". "
-            row = []
+            element: string = ". "
+            row: list(string) = []
             for j in range(self.screen_width):
                 row.append(element)
             self.screen_body.append(row)
             
       
-    def display_screen(self):
+    def display_screen(self) -> None:
         
-        x = self.screen_width
-        y = self.screen_height
+        x: int = self.screen_width
+        y: int = self.screen_height
         
         self.set_screen_body()
         
@@ -53,16 +63,40 @@ class Tetris:
     def __init__(self):
         pass
     
-    
-    def run(self):
+    def move(self):
         pass
+        
     
     
-def main():
-    screen: Screen = Screen()
+    def run(self) -> None:
+        running: bool = True
+        
+        block: Block = Block()
+        screen: Screen = Screen()
+        screen_list: list = screen.screen_body
+        
+        block = block.block
+        x: int = screen.screen_width
+        y: int = screen.screen_height
+        mid: int = x // 2
     
-    screen.display_screen()
+        screen.display_screen()
+        
+        while(running):
+            for i in range(0, y, 1):
+                 temp = screen_list[i][mid]
+                 screen_list[i][mid] = block
+                 clear()
+                 screen.display_screen()
+                 screen_list[i][mid] = temp
+                 delay()
+               
+            running = False
+
+    
+def main() -> None:
+    tetris: Tetris = Tetris()
+    tetris.run()
     
 if __name__ == '__main__':
     main()
-    
